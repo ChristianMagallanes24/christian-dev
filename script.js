81e4292197a19edbc5b223c3ec60206bb34711d1
christian magallanes <desarrollowebfront@gmail.com>
2024-09-27 22:46:00 -0300

    aa

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const navButton = document.querySelector('.nav-mobile');
 const navBar = document.querySelector('.navBar');
+const box = document.getElementById("box");
+const test = document.getElementById('test');
 
 navButton.addEventListener('click', () => {
   navBar.classList.toggle('active');
 });
+
